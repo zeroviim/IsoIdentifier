0310ee246fe77f6871f845e51fae6c6038458500
zeroviim <43391837+zeroviim@users.noreply.github.com>
2019-04-01 22:11:43 -0400

    Add files via upload

---
+++ ISOIdentifier/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ISOIdentifier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Open = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lsbx_Sector16 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btn_Open
+            // 
+            this.btn_Open.Location = new System.Drawing.Point(12, 44);
+            this.btn_Open.Name = "btn_Open";
+            this.btn_Open.Size = new System.Drawing.Size(75, 23);
+            this.btn_Open.TabIndex = 0;
+            this.btn_Open.Text = "Open";
+            this.btn_Open.UseVisualStyleBackColor = true;
+            this.btn_Open.Click += new System.EventHandler(this.btn_Open_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(124, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sector 16 Information";
+            // 
+            // lsbx_Sector16
+            // 
+            this.lsbx_Sector16.FormattingEnabled = true;
+            this.lsbx_Sector16.Location = new System.Drawing.Point(127, 44);
+            this.lsbx_Sector16.Name = "lsbx_Sector16";
+            this.lsbx_Sector16.Size = new System.Drawing.Size(185, 147);
+            this.lsbx_Sector16.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 483);
+            this.Controls.Add(this.lsbx_Sector16);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Open);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Open;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lsbx_Sector16;
+    }
+}
+
---
+++ ISOIdentifier/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ISOIdentifier
+{
+    public partial class Form1 : Form
+    {
+        string romLocation = @"C:\Users\Michael\Documents\Tomba stuff\Tomba! (USA).bin";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Open_Click(object sender, EventArgs e)
+        {
+            BinaryReader rom = new BinaryReader(File.OpenRead(romLocation));
+            BigDataExtract(rom, out string[] sector16Info);
+            for (int i = 0; i < sector16Info.Length; i++)
+            {
+                lsbx_Sector16.Items.Add(sector16Info[i].ToString());
+            }
+        }
+
+        private void BigDataExtract(BinaryReader rom, out string[] sector16Info)
+        {
+            //starting variables
+            List<string> methodInformation = new List<string>();
+            int romPosition = 0x9300; //sector 16 TODO:start at sector 4 to check what region it is based on license string to export
+            string information = "";
+            int infoLength = 0;
+
+            //string pulling
+            romPosition = 0x9319; //standard identifier
+            infoLength = 5;
+            ReadIsoString(rom, romPosition, infoLength, out information);
+            methodInformation.Add(information);
+            romPosition += infoLength;
+            infoLength = 1;
+            ReadIso(rom, romPosition, infoLength, out information);
+            methodInformation.Add(information);
+            
+            //assigning list to out array
+            sector16Info = methodInformation.ToArray();
+        }
+
+        private void ReadIsoString(BinaryReader rom, int romPosition, int infoLength, out string information)
+        {
+            rom.BaseStream.Position = romPosition;
+            char[] output = rom.ReadChars(infoLength);
+            information = new string(output);
+        }
+
+        //private void Export()
+        //{
+        //  write Region:{0}, sector16Info[0].tostriong, etc
+        //}
+
+        //notes repository
+
+        //sector 4: license info
+        //sectors 5-11: playstation logo (used to compare with the bios as a piracy check)
+        //sector 16: primary volume description
+
+        //0C = minutes (01-26, increments on 0D = 59)
+        //0D = seconds (00-59, increments on 0E = 75)
+        //0E = sectors (00-74)
+
+        //0F = appears to always be 02h, not sure why, doesnt seem to line up with frames despite being the next part of the readout
+
+        //sector 17: volume descriptor set terminator
+
+        //first 00 02 00 02 (0C-0F at start of sector)
+        //max 26 30 29 02 may be 119129 not 119130(last sector according to hxd)
+    }
+}
